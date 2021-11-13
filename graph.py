94861d5b8d6807fdec1e25bca263e0cb24c12059
Yaros5 <94068316+Yaros5@users.noreply.github.com>
2021-11-13 16:30:33 +0200

    com

---
+++ graph.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+list_1 = list(range(-1000, 1000))
+list_2 = []
+plt.style.use('seaborn')
+for x in range(-1000, 1000):
+    res = x ** 2
+    list_2.append(res)
+fig, ax = plt.subplots()
+ax.plot(list_1, list_2, c = (0.1, 0.4, .03))
+plt.show()
