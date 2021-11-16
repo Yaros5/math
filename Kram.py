af047205f92a9c55bfecedc033fd3bb9235ae907
Oleshkooo <85068263+Oleshkooo@users.noreply.github.com>
2021-11-16 16:36:06 +0200

    Kram

---
+++ Kram.py
@@ -0,0 +1,351 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Kram.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(477, 532)
+        MainWindow.setStyleSheet("background: #34495e")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(30, 50, 271, 381))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        self.spinBox_8 = QtWidgets.QSpinBox(self.gridLayoutWidget)
+        self.spinBox_8.setStyleSheet(".QPushButton {\n"
+"  border: none;\n"
+"  background: #ecf0f1;\n"
+"  color: #000;\n"
+"  border-radius: 20px;\n"
+"  width: 100px;\n"
+"  height: 50px;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QPushButton:hover {\n"
+"  background: #bdc3c7;\n"
+"}\n"
+"\n"
+".QPushButton:pressed {\n"
+"  width: 95px;\n"
+"  height: 18px;\n"
+"  font-size: 14px;\n"
+"}")
+        self.spinBox_8.setObjectName("spinBox_8")
+        self.gridLayout.addWidget(self.spinBox_8, 1, 4, 1, 1)
+        self.spinBox_4 = QtWidgets.QSpinBox(self.gridLayoutWidget)
+        self.spinBox_4.setStyleSheet(".QPushButton {\n"
+"  border: none;\n"
+"  background: #ecf0f1;\n"
+"  color: #000;\n"
+"  border-radius: 20px;\n"
+"  width: 100px;\n"
+"  height: 50px;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QPushButton:hover {\n"
+"  background: #bdc3c7;\n"
+"}\n"
+"\n"
+".QPushButton:pressed {\n"
+"  width: 95px;\n"
+"  height: 18px;\n"
+"  font-size: 14px;\n"
+"}")
+        self.spinBox_4.setObjectName("spinBox_4")
+        self.gridLayout.addWidget(self.spinBox_4, 2, 3, 1, 1)
+        self.spinBox_9 = QtWidgets.QSpinBox(self.gridLayoutWidget)
+        self.spinBox_9.setStyleSheet(".QPushButton {\n"
+"  border: none;\n"
+"  background: #ecf0f1;\n"
+"  color: #000;\n"
+"  border-radius: 20px;\n"
+"  width: 100px;\n"
+"  height: 50px;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QPushButton:hover {\n"
+"  background: #bdc3c7;\n"
+"}\n"
+"\n"
+".QPushButton:pressed {\n"
+"  width: 95px;\n"
+"  height: 18px;\n"
+"  font-size: 14px;\n"
+"}")
+        self.spinBox_9.setObjectName("spinBox_9")
+        self.gridLayout.addWidget(self.spinBox_9, 1, 5, 1, 1)
+        self.spinBox_7 = QtWidgets.QSpinBox(self.gridLayoutWidget)
+        self.spinBox_7.setStyleSheet(".QPushButton {\n"
+"  border: none;\n"
+"  background: #ecf0f1;\n"
+"  color: #000;\n"
+"  border-radius: 20px;\n"
+"  width: 100px;\n"
+"  height: 50px;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QPushButton:hover {\n"
+"  background: #bdc3c7;\n"
+"}\n"
+"\n"
+".QPushButton:pressed {\n"
+"  width: 95px;\n"
+"  height: 18px;\n"
+"  font-size: 14px;\n"
+"}")
+        self.spinBox_7.setObjectName("spinBox_7")
+        self.gridLayout.addWidget(self.spinBox_7, 1, 3, 1, 1)
+        self.spinBox_5 = QtWidgets.QSpinBox(self.gridLayoutWidget)
+        self.spinBox_5.setStyleSheet(".QPushButton {\n"
+"  border: none;\n"
+"  background: #ecf0f1;\n"
+"  color: #000;\n"
+"  border-radius: 20px;\n"
+"  width: 100px;\n"
+"  height: 50px;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QPushButton:hover {\n"
+"  background: #bdc3c7;\n"
+"}\n"
+"\n"
+".QPushButton:pressed {\n"
+"  width: 95px;\n"
+"  height: 18px;\n"
+"  font-size: 14px;\n"
+"}")
+        self.spinBox_5.setObjectName("spinBox_5")
+        self.gridLayout.addWidget(self.spinBox_5, 2, 4, 1, 1)
+        self.spinBox_6 = QtWidgets.QSpinBox(self.gridLayoutWidget)
+        self.spinBox_6.setStyleSheet(".QPushButton {\n"
+"  border: none;\n"
+"  background: #ecf0f1;\n"
+"  color: #000;\n"
+"  border-radius: 20px;\n"
+"  width: 100px;\n"
+"  height: 50px;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QPushButton:hover {\n"
+"  background: #bdc3c7;\n"
+"}\n"
+"\n"
+".QPushButton:pressed {\n"
+"  width: 95px;\n"
+"  height: 18px;\n"
+"  font-size: 14px;\n"
+"}")
+        self.spinBox_6.setObjectName("spinBox_6")
+        self.gridLayout.addWidget(self.spinBox_6, 2, 5, 1, 1)
+        self.spinBox = QtWidgets.QSpinBox(self.gridLayoutWidget)
+        self.spinBox.setStyleSheet(".QPushButton {\n"
+"  border: none;\n"
+"  background: #ecf0f1;\n"
+"  color: #000;\n"
+"  border-radius: 20px;\n"
+"  width: 100px;\n"
+"  height: 50px;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QPushButton:hover {\n"
+"  background: #bdc3c7;\n"
+"}\n"
+"\n"
+".QPushButton:pressed {\n"
+"  width: 95px;\n"
+"  height: 18px;\n"
+"  font-size: 14px;\n"
+"}")
+        self.spinBox.setObjectName("spinBox")
+        self.gridLayout.addWidget(self.spinBox, 0, 3, 1, 1)
+        self.spinBox_2 = QtWidgets.QSpinBox(self.gridLayoutWidget)
+        self.spinBox_2.setStyleSheet(".QPushButton {\n"
+"  border: none;\n"
+"  background: #ecf0f1;\n"
+"  color: #000;\n"
+"  border-radius: 20px;\n"
+"  width: 100px;\n"
+"  height: 50px;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QPushButton:hover {\n"
+"  background: #bdc3c7;\n"
+"}\n"
+"\n"
+".QPushButton:pressed {\n"
+"  width: 95px;\n"
+"  height: 18px;\n"
+"  font-size: 14px;\n"
+"}")
+        self.spinBox_2.setObjectName("spinBox_2")
+        self.gridLayout.addWidget(self.spinBox_2, 0, 4, 1, 1)
+        self.spinBox_3 = QtWidgets.QSpinBox(self.gridLayoutWidget)
+        self.spinBox_3.setStyleSheet(".QPushButton {\n"
+"  border: none;\n"
+"  background: #ecf0f1;\n"
+"  color: #000;\n"
+"  border-radius: 20px;\n"
+"  width: 100px;\n"
+"  height: 50px;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QPushButton:hover {\n"
+"  background: #bdc3c7;\n"
+"}\n"
+"\n"
+".QPushButton:pressed {\n"
+"  width: 95px;\n"
+"  height: 18px;\n"
+"  font-size: 14px;\n"
+"}")
+        self.spinBox_3.setObjectName("spinBox_3")
+        self.gridLayout.addWidget(self.spinBox_3, 0, 5, 1, 1)
+        self.gridLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
+        self.gridLayoutWidget_2.setGeometry(QtCore.QRect(340, 50, 101, 381))
+        self.gridLayoutWidget_2.setObjectName("gridLayoutWidget_2")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.gridLayoutWidget_2)
+        self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.spinBox_11 = QtWidgets.QSpinBox(self.gridLayoutWidget_2)
+        self.spinBox_11.setStyleSheet(".QPushButton {\n"
+"  border: none;\n"
+"  background: #ecf0f1;\n"
+"  color: #000;\n"
+"  border-radius: 20px;\n"
+"  width: 100px;\n"
+"  height: 50px;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QPushButton:hover {\n"
+"  background: #bdc3c7;\n"
+"}\n"
+"\n"
+".QPushButton:pressed {\n"
+"  width: 95px;\n"
+"  height: 18px;\n"
+"  font-size: 14px;\n"
+"}")
+        self.spinBox_11.setObjectName("spinBox_11")
+        self.gridLayout_2.addWidget(self.spinBox_11, 1, 0, 1, 1)
+        self.spinBox_10 = QtWidgets.QSpinBox(self.gridLayoutWidget_2)
+        self.spinBox_10.setStyleSheet(".QPushButton {\n"
+"  border: none;\n"
+"  background: #ecf0f1;\n"
+"  color: #000;\n"
+"  border-radius: 20px;\n"
+"  width: 100px;\n"
+"  height: 50px;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QPushButton:hover {\n"
+"  background: #bdc3c7;\n"
+"}\n"
+"\n"
+".QPushButton:pressed {\n"
+"  width: 95px;\n"
+"  height: 18px;\n"
+"  font-size: 14px;\n"
+"}")
+        self.spinBox_10.setObjectName("spinBox_10")
+        self.gridLayout_2.addWidget(self.spinBox_10, 0, 0, 1, 1)
+        self.spinBox_12 = QtWidgets.QSpinBox(self.gridLayoutWidget_2)
+        self.spinBox_12.setStyleSheet(".QPushButton {\n"
+"  border: none;\n"
+"  background: #ecf0f1;\n"
+"  color: #000;\n"
+"  border-radius: 20px;\n"
+"  width: 100px;\n"
+"  height: 50px;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QPushButton:hover {\n"
+"  background: #bdc3c7;\n"
+"}\n"
+"\n"
+".QPushButton:pressed {\n"
+"  width: 95px;\n"
+"  height: 18px;\n"
+"  font-size: 14px;\n"
+"}")
+        self.spinBox_12.setObjectName("spinBox_12")
+        self.gridLayout_2.addWidget(self.spinBox_12, 2, 0, 1, 1)
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(130, 80, 81, 17))
+        self.label.setStyleSheet("color: #fff\n"
+"")
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(350, 80, 81, 17))
+        self.label_2.setStyleSheet("color: #fff\n"
+"")
+        self.label_2.setObjectName("label_2")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(170, 420, 131, 41))
+        self.pushButton.setStyleSheet(".QPushButton {\n"
+"  border: none;\n"
+"  background: #ecf0f1;\n"
+"  color: #000;\n"
+"  border-radius: 20px;\n"
+"  width: 200px;\n"
+"  height: 100px;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QPushButton:hover {\n"
+"  background: #bdc3c7;\n"
+"}\n"
+"\n"
+".QPushButton:pressed {\n"
+"  width: 95px;\n"
+"  height: 18px;\n"
+"  font-size: 14px;\n"
+"}")
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Матриця A"))
+        self.label_2.setText(_translate("MainWindow", "Матриця B"))
+        self.pushButton.setText(_translate("MainWindow", "Обчислити"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
