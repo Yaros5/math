3ae189c5e2a76e1f27f46b0a618963a0793b6eb9
Oleshkooo <85068263+Oleshkooo@users.noreply.github.com>
2021-11-16 17:28:26 +0200

    Trans

---
+++ Trans.py
@@ -0,0 +1,252 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Reversed.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(655, 515)
+        MainWindow.setStyleSheet("background: #34495e")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(130, 80, 81, 17))
+        self.label.setStyleSheet("color: #fff\n"
+"")
+        self.label.setObjectName("label")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(230, 410, 131, 41))
+        self.pushButton.setStyleSheet(".QPushButton {\n"
+"  border: none;\n"
+"  background: #ecf0f1;\n"
+"  color: #000;\n"
+"  border-radius: 20px;\n"
+"  width: 200px;\n"
+"  height: 100px;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QPushButton:hover {\n"
+"  background: #bdc3c7;\n"
+"}\n"
+"\n"
+".QPushButton:pressed {\n"
+"  width: 95px;\n"
+"  height: 18px;\n"
+"  font-size: 14px;\n"
+"}")
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(40, 130, 231, 80))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.lineEdit = QtWidgets.QLineEdit(self.horizontalLayoutWidget)
+        self.lineEdit.setStyleSheet(".QLineEdit {\n"
+"  color: #fff;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QLineEdit:hover {\n"
+"  background: #2c3e50;\n"
+"}")
+        self.lineEdit.setObjectName("lineEdit")
+        self.horizontalLayout.addWidget(self.lineEdit)
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.horizontalLayoutWidget)
+        self.lineEdit_2.setStyleSheet(".QLineEdit {\n"
+"  color: #fff;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QLineEdit:hover {\n"
+"  background: #2c3e50;\n"
+"}")
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.horizontalLayout.addWidget(self.lineEdit_2)
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.horizontalLayoutWidget)
+        self.lineEdit_3.setStyleSheet(".QLineEdit {\n"
+"  color: #fff;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QLineEdit:hover {\n"
+"  background: #2c3e50;\n"
+"}")
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.horizontalLayout.addWidget(self.lineEdit_3)
+        self.horizontalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalLayoutWidget_2.setGeometry(QtCore.QRect(40, 210, 231, 80))
+        self.horizontalLayoutWidget_2.setObjectName("horizontalLayoutWidget_2")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget_2)
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.lineEdit_4 = QtWidgets.QLineEdit(self.horizontalLayoutWidget_2)
+        self.lineEdit_4.setStyleSheet(".QLineEdit {\n"
+"  color: #fff;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QLineEdit:hover {\n"
+"  background: #2c3e50;\n"
+"}")
+        self.lineEdit_4.setObjectName("lineEdit_4")
+        self.horizontalLayout_2.addWidget(self.lineEdit_4)
+        self.lineEdit_5 = QtWidgets.QLineEdit(self.horizontalLayoutWidget_2)
+        self.lineEdit_5.setStyleSheet(".QLineEdit {\n"
+"  color: #fff;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QLineEdit:hover {\n"
+"  background: #2c3e50;\n"
+"}")
+        self.lineEdit_5.setObjectName("lineEdit_5")
+        self.horizontalLayout_2.addWidget(self.lineEdit_5)
+        self.lineEdit_6 = QtWidgets.QLineEdit(self.horizontalLayoutWidget_2)
+        self.lineEdit_6.setStyleSheet(".QLineEdit {\n"
+"  color: #fff;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QLineEdit:hover {\n"
+"  background: #2c3e50;\n"
+"}")
+        self.lineEdit_6.setObjectName("lineEdit_6")
+        self.horizontalLayout_2.addWidget(self.lineEdit_6)
+        self.horizontalLayoutWidget_3 = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalLayoutWidget_3.setGeometry(QtCore.QRect(40, 290, 231, 80))
+        self.horizontalLayoutWidget_3.setObjectName("horizontalLayoutWidget_3")
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget_3)
+        self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.lineEdit_7 = QtWidgets.QLineEdit(self.horizontalLayoutWidget_3)
+        self.lineEdit_7.setStyleSheet(".QLineEdit {\n"
+"  color: #fff;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QLineEdit:hover {\n"
+"  background: #2c3e50;\n"
+"}")
+        self.lineEdit_7.setObjectName("lineEdit_7")
+        self.horizontalLayout_3.addWidget(self.lineEdit_7)
+        self.lineEdit_8 = QtWidgets.QLineEdit(self.horizontalLayoutWidget_3)
+        self.lineEdit_8.setStyleSheet(".QLineEdit {\n"
+"  color: #fff;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QLineEdit:hover {\n"
+"  background: #2c3e50;\n"
+"}")
+        self.lineEdit_8.setObjectName("lineEdit_8")
+        self.horizontalLayout_3.addWidget(self.lineEdit_8)
+        self.lineEdit_9 = QtWidgets.QLineEdit(self.horizontalLayoutWidget_3)
+        self.lineEdit_9.setStyleSheet(".QLineEdit {\n"
+"  color: #fff;\n"
+"  font-size: 15px;\n"
+"}\n"
+"\n"
+".QLineEdit:hover {\n"
+"  background: #2c3e50;\n"
+"}")
+        self.lineEdit_9.setObjectName("lineEdit_9")
+        self.horizontalLayout_3.addWidget(self.lineEdit_9)
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(400, 90, 81, 17))
+        self.label_2.setStyleSheet("color: #fff\n"
+"")
+        self.label_2.setObjectName("label_2")
+        self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(320, 130, 231, 241))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        self.label_8 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_8.setStyleSheet("color: #fff\n"
+"")
+        self.label_8.setText("")
+        self.label_8.setObjectName("label_8")
+        self.gridLayout.addWidget(self.label_8, 1, 2, 1, 1)
+        self.label_7 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_7.setStyleSheet("color: #fff\n"
+"")
+        self.label_7.setText("")
+        self.label_7.setObjectName("label_7")
+        self.gridLayout.addWidget(self.label_7, 1, 1, 1, 1)
+        self.label_4 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_4.setStyleSheet("color: #fff\n"
+"")
+        self.label_4.setText("")
+        self.label_4.setObjectName("label_4")
+        self.gridLayout.addWidget(self.label_4, 0, 1, 1, 1)
+        self.label_6 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_6.setStyleSheet("color: #fff\n"
+"")
+        self.label_6.setText("")
+        self.label_6.setObjectName("label_6")
+        self.gridLayout.addWidget(self.label_6, 1, 0, 1, 1)
+        self.label_3 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_3.setStyleSheet("color: #fff\n"
+"")
+        self.label_3.setText("")
+        self.label_3.setObjectName("label_3")
+        self.gridLayout.addWidget(self.label_3, 0, 0, 1, 1)
+        self.label_5 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_5.setStyleSheet("color: #fff\n"
+"")
+        self.label_5.setText("")
+        self.label_5.setObjectName("label_5")
+        self.gridLayout.addWidget(self.label_5, 0, 2, 1, 1)
+        self.label_9 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_9.setStyleSheet("color: #fff\n"
+"")
+        self.label_9.setText("")
+        self.label_9.setObjectName("label_9")
+        self.gridLayout.addWidget(self.label_9, 2, 0, 1, 1)
+        self.label_10 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_10.setStyleSheet("color: #fff\n"
+"")
+        self.label_10.setText("")
+        self.label_10.setObjectName("label_10")
+        self.gridLayout.addWidget(self.label_10, 2, 1, 1, 1)
+        self.label_11 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_11.setStyleSheet("color: #fff\n"
+"")
+        self.label_11.setText("")
+        self.label_11.setObjectName("label_11")
+        self.gridLayout.addWidget(self.label_11, 2, 2, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Матриця A"))
+        self.pushButton.setText(_translate("MainWindow", "Обчислити"))
+        self.label_2.setText(_translate("MainWindow", "Відповідь:"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
