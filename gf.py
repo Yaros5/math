8d7926b15000fa911bb3e38a7fdef817ef43c0c5
Yaros5 <94068316+Yaros5@users.noreply.github.com>
2021-11-16 17:06:22 +0200

    COM

--- gf.py
+++ gf.py
@@ -15,32 +15,16 @@ MainWindow.show()
 # Прописуєм логіку кнопок
 
 def bp():
-    MainWindow.hide()
-    MainWindow = QtWidgets.QCalcDeterminant()
-    ui = Ui_CalcDeterminant()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+
 
 def bp_2():
-    MainWindow.hide()
-    MainWindow = QtWidgets.QCalcInverted()
-    ui = Ui_CalcInverted()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+
 
 def bp_3():
-    MainWindow.hide()
-    MainWindow = QtWidgets.QCalcKramar()
-    ui = Ui_CalcKramar()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+
 
 def bp_4():
-    MainWindow.hide()
-    MainWindow = QtWidgets.QCalcTranspont()
-    ui = Ui_CalcTranspont()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+
 
 
 ui.pushButton.clicked.connect(bp)
